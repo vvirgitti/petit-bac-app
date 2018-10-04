15693a6a05c8c6373aba92caa501645bbbbe22e6
Vanessa Virgitti <virgitti@gmail.com>
2018-10-04 15:05:04 +0100

    Added an http call to a movie api

--- bac.go
+++ bac.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"fmt"
 	"math/rand"
@@ -68,6 +70,53 @@ func Answer(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		}
 	}
 	fmt.Println("Answer ", answer)
+
+	movieApiUrl := "https://api.themoviedb.org/3/search/movie?include_adult=false&api_key="
+	apiKey :=  Credentials["movieApiKey"]
+
+	sanitisedAnswer := strings.Replace(answer, " ", "%2B", -1)
+	
+	url := movieApiUrl + apiKey + "&language=en&" + "query=" + sanitisedAnswer
+
+	fmt.Println("URL", url)
+
+	payload := strings.NewReader("{}")
+
+	req, _ := http.NewRequest("GET", url, payload)
+	res, _ := http.DefaultClient.Do(req)
+
+	body, _ := ioutil.ReadAll(res.Body)
+
+	var response = new(MovieReponse)
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		fmt.Println("Could not unmarshal the response from the API", err)
+	}
+
+	var movieList []string
+
+	for _, movie := range response.Results {
+		movieList = append(movieList, strings.ToLower(movie.Title))
+	}
+
+	fmt.Println("Movie List", movieList)
+
+	if Contains(movieList, answer) {
+		fmt.Println("Success")
+	} else {
+		fmt.Println("Bad answer")
+	}
+
+	defer res.Body.Close()
+}
+
+func Contains(a []string, x string) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
@@ -78,4 +127,26 @@ func main() {
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 
+}
+
+type MovieReponse struct {
+	Page         int `json:"page"`
+	TotalResults int `json:"total_results"`
+	TotalPages   int `json:"total_pages"`
+	Results      []struct {
+		VoteCount        int     `json:"vote_count"`
+		ID               int     `json:"id"`
+		Video            bool    `json:"video"`
+		VoteAverage      float64 `json:"vote_average"`
+		Title            string  `json:"title"`
+		Popularity       float64 `json:"popularity"`
+		PosterPath       string  `json:"poster_path"`
+		OriginalLanguage string  `json:"original_language"`
+		OriginalTitle    string  `json:"original_title"`
+		GenreIds         []int   `json:"genre_ids"`
+		BackdropPath     string  `json:"backdrop_path"`
+		Adult            bool    `json:"adult"`
+		Overview         string  `json:"overview"`
+		ReleaseDate      string  `json:"release_date"`
+	} `json:"results"`
 }
